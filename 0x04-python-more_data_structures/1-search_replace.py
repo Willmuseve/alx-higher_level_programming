e8fbf8be9bf9ae2c377f2ed39d294058f0540e59
Willmuseve <willmuseve@gmail.com>
2023-03-15 13:03:20 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     # Create a new list to store the result
     result = []
-    
+
     # Loop over each element of the input list
     for element in my_list:
         # If the element matches the search value, replace it with the new value
@@ -11,6 +11,6 @@ def search_replace(my_list, search, replace):
             result.append(replace)
         else:
             result.append(element)
-    
+
     # Return the new list
     return result
