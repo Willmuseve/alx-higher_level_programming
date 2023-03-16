723e84972e437ef53ded06027dc6815b98b0aa85
Willmuseve <willmuseve@gmail.com>
2023-03-15 19:47:51 -0700

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,9 @@
 def common_elements(set_1, set_2):
     common_set = set()
     for elem in set_1:
-        if elem in set_2:
+        if elem not in set_2:
+            common_set.add(elem)
+    for elem in set_2:
+        if elem not in set_1:
             common_set.add(elem)
     return common_set
