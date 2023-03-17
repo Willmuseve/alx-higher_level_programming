3e977530dd64077faca535f8a9dd29512c590730
Willmuseve <willmuseve@gmail.com>
2023-03-16 23:55:24 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best_key = None
-    best_value = float('-inf')
-    for key, value in a_dictionary.items():
-        if isinstance(value, int) and value > best_value:
-            best_key = key
-            best_value = value
-    return best_key
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
