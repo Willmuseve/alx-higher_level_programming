a7bad32cdf830c436905f59628a588dbc87eb35e
will museve <willmuseve@gmail.com>
2023-03-27 14:54:08 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while True:
+            if i == x:
+                break
+            print(my_list[i], end=' ')
+            i += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return i
