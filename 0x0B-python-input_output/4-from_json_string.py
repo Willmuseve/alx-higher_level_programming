ab90e7701b05aadf60904a43ac802d34071cf099
will museve <willmuseve@gmail.com>
2023-04-13 12:41:02 -0700

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""  function that returns an object (Python data structure) represented by a JSON string"""
+"""
+ function that returns an object (Python data structure) represented by a JSON string
+"""
 
 import json
 
 
 def from_json_string(my_str):
-    """Object string"""
+    """object represented by a JSON string"""
     return json.loads(my_str)
