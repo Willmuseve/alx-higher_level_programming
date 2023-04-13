19b658a65f5ee75ddf9bad86e90336522f93bf64
will museve <willmuseve@gmail.com>
2023-04-13 12:03:20 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """This module defines a text-reading function"""
 
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as o:
-        print(o.read(), end="")
+    with open(filename, encoding="UTF-8") as f:
+        print(f.read(), end="")
 
