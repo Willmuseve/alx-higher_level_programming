d350c4573f2c6f61e6bd70a0517e7db3cb553a4c
will museve <willmuseve@gmail.com>
2023-04-13 12:29:16 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This functions returns the JSON representation of a string"""
+import json
+
+
+def to_json_string(my_obj):
+    """the JSon rep"""
+    return json.dumps(my_obj)
