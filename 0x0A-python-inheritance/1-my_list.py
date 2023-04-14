965c196d3bfd8849e81afe74d2b79e565bf01830
will museve <willmuseve@gmail.com>
2023-04-13 20:08:02 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+ class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    def __init__(self):
+
+        super().__init__()
+
+    def print_sorted(self):
+
+        print(sorted(self))
