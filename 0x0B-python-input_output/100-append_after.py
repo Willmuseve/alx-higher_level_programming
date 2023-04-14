75a1b41eb8a3186c7d46f285a208afb55ba4259d
will museve <willmuseve@gmail.com>
2023-04-13 19:00:58 -0700

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  function that inserts a line of text to a file, after each line containing a specific string """
+"""  function that inserts a line of text to a file,
+after each line containing a specific string """
 
 
 def append_after(filename="", search_string="", new_string=""):
