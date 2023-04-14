9d2fd5389552a96a34165a870c736ced628e121f
will museve <willmuseve@gmail.com>
2023-04-13 20:05:07 -0700

    Look up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" a function that returns the list of available attributes and methods of an object """
+""" a function that returns the list of available
+attributes and methods of an object """
 
 
 def lookup(obj):
-    """"""
+    """ Returns a list of attributes """
     return dir(obj)
