810a567c0569a864919cf5af605b629e117dcc3a
will museve <willmuseve@gmail.com>
2023-04-16 04:06:43 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if object  is an instance of a class"""
+
+    return (isinstance(obj, a_class))
