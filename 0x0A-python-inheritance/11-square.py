aaed4528dcfe065fa7eec9baa3e655adcce2f696
will museve <willmuseve@gmail.com>
2023-04-16 04:53:37 -0700

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,53 @@
 #!/usr/bin/python3
-"""contains a Rectangle subclass Square"""
-Rectangle = __import__('9-rectangle').Rectangle
+"""
+Contains subclass Rectangle and square
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A rep of a rectangle"""
+    def __init__(self, width, height):
+        """initialization of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """informal string rep of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """a square"""
-
+    """A rep a square"""
     def __init__(self, size):
-        """Initialize a square
-        """
+        """initialization of the square"""
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
-        self.__size = 
+
+    def area(self):
+        """"returns the square area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string rep of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
