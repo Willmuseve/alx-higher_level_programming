8a7130c8d0348781a6b901a6f3f85481e80c383f
Willmuseve <104129182+Willmuseve@users.noreply.github.com>
2023-05-08 00:04:23 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a function that appends a file"""
-
-
-def append_write(filename, text=""):
-    """Append"""
-    with open(filename, "a", encoding="UTF-8") as f:
-        return f.write(text)
--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This functions returns the JSON representation of a string"""
-import json
-
-
-def to_json_string(my_obj):
-    """the JSon rep"""
-    return json.dumps(my_obj)
