bbd004db502520e25ad2fae18a3dbbd809b97410
Willmuseve <willmuseve@gmail.com>
2023-06-12 20:09:07 -0700

    12. My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """inherits here"""
+
+    def __eq__(self, value):
+        """=="""
+        return self.real != value
+
+    def __ne__(self, value):
+        """!="""
+        return self.real == 
