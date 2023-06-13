532cbe0a6f72a7db7f051e65242117efc978726a
Willmuseve <willmuseve@gmail.com>
2023-06-12 19:44:26 -0700

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" An empty class basegeometry"""
+
+
+class BaseGeometry:
+    """instance of the empty geometry class"""
+    def __init__(self):
+        """initialize class"""
+        pass
