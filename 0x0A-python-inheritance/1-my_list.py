5ebebb1f7321f98125ea4de1615158855e9ead38
Willmuseve <willmuseve@gmail.com>
2023-06-12 19:14:42 -0700

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,12 +2,6 @@
 class MyList(list):
 """defines class MyList that inherits from list and has a public method print_sorted()"""
 
-  def print_sorted(self):
-    """
-    Prints the list, but sorted (ascending sort).
-    """
 
-    self.sort()
-    for item in self:
-      print(item, end=" ")
-    print()
+    def print_sorted(self):
+        print(sorted(self))
