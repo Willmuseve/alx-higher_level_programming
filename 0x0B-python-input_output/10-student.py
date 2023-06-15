d78009451f01a70dad27c2b3488bc2422ef8ffcf
Willmuseve <willmuseve@gmail.com>
2023-06-14 19:43:57 -0700

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  a class Student that defines a student"""
+
+
+class Student:
+    """ a class student with public
+    instance variables"""
+    def __init__(self, first_name, last_name, age):
+        """ Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """public method that retrieves a dictionary representation """
+        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
