9b83252988124450233d50d5e2683b2b8d49939f
Willmuseve <willmuseve@gmail.com>
2023-07-18 17:56:14 -0700

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 --script that ceates a table called first_table in the currrent database
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
