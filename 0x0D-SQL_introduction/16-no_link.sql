92dd5070d481038150aa93e552b538d13b4f72d6
Willmuseve <willmuseve@gmail.com>
2023-07-18 18:53:15 -0700

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,7 @@
 --a script that lists all records of the table second_table
-SELECT score, name
-FROM second_table
-WHERE `name` IS NOT NULL
-ORDER BY score DESC;
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to the mysql command
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
