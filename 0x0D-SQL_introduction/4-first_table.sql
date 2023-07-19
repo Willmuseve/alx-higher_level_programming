8db316b1cf4e803f0b5ae4dd68ceb7caed6987fb
Willmuseve <willmuseve@gmail.com>
2023-07-18 18:01:33 -0700

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,8 @@
 --script that ceates a table called first_table in the currrent database
+-- The database name will be passed as an argument of the mysql command
+-- first_table description:
+--             id INT
+--             name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
