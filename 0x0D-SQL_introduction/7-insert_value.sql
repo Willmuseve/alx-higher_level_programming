e1f7ed9cfa4b997e973fd28883745ff83c29d60a
Willmuseve <willmuseve@gmail.com>
2023-07-18 19:27:14 -0700

    First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
---a script that inserts a new row in the table first_table (database hbtn_0c_0)
+-- inserts a new row in the table first_table (database hbtn_0c_0).
+--new row will contain id of 89 and name best school
+--The database name will be passed as an argumentof the mysql command
 INSERT INTO `first_table` (`id`, `name`) VALUES(89, "Best School");
