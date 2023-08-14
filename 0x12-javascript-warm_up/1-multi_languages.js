388643d8cf1795e3092d3a7f65a62b06872f9c40
Willmuseve <willmuseve@gmail.com>
2023-08-14 19:59:12 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -8,7 +8,6 @@
 const cIsFun = 'C is fun';
 const pythonIsCool = 'Python is cool';
 const javascriptIsAmazing = 'JavaScript is amazing';
-
 console.log(cIsFun);
 console.log(pythonIsCool);
 console.log(javascriptIsAmazing);
