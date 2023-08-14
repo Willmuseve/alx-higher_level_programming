2b4bc469185ef3499710ba33c8114f613f6c1aa0
Willmuseve <willmuseve@gmail.com>
2023-08-14 19:54:44 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,9 +2,9 @@
 
 // prints the named 3 lines
 
-const cIsFun = "C is fun";
-const pythonIsCool = "Python is cool";
-const javascriptIsAmazing = "JavaScript is amazing";
+const cIsFun = 'C is fun';
+const pythonIsCool = 'Python is cool';
+const javascriptIsAmazing = 'JavaScript is amazing';
 
 console.log(cIsFun);
 console.log(pythonIsCool);
