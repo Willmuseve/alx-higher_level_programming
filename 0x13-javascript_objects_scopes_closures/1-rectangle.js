a979f9a8f82b4eece8427d8e686669620b444a2d
Willmuseve <willmuseve@gmail.com>
2023-08-15 18:45:11 +0000

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+//rectangle class
+//constructor with two arguments w and h
+// instance attribute width with the value of w
+//  instance attribute height with the value of h
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
