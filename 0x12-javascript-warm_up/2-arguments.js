c44c01eaeec89ba1b93c3c72b04a0cff7453a928
Willmuseve <willmuseve@gmail.com>
2023-08-15 01:47:11 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 // prints a message depending of the number of arguments passed
 
 if (process.argv.length > 3) {
-  console.log('Arguments found
+  console.log('Arguments found');
 } else if (process.argv.lenght === 3) {
   console.log('Argument found');
 } else {
