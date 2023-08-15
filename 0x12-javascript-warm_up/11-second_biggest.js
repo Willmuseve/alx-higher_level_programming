1c63b921d5690770f4e1862c612dd7b94555ae06
Willmuseve <willmuseve@gmail.com>
2023-08-15 02:38:17 +0000

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const arguments = process.argv
-    .map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
+  const arguments = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
   console.log(arguments[arguments.length - 2]);
 }
