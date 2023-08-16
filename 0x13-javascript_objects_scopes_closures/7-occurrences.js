50e6b9c1c3c2c3240bad2e676bcef871afb0863a
Willmuseve <willmuseve@gmail.com>
2023-08-16 02:05:51 +0000

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  list.forEach(search => {
+    if (search === searchElement) {
+      count++;
+    }
+  });
+
+  return (count);
+};
