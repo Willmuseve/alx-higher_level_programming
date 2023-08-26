4332acaa049f6447a44048f6c015f1f11ce8c0a7
Willmuseve <willmuseve@gmail.com>
2023-08-26 12:58:57 -0700

    Delete States

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa 
+A script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
 """
 
 from sys import argv
