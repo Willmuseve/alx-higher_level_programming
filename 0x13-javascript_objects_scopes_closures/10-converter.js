638ae48386ef81a507fad4d29c007d626f44d56a
Wilson Museve <104129182+Willmuseve@users.noreply.github.com>
2023-10-02 04:14:25 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  return function (b) { return b.toString(base); };
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let p = 0;
-
-exports.logMe = function (item) {
-  console.log(`${p}: ${item}`);
-  p += 1;
-};
