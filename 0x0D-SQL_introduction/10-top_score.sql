0e5efe36aaf2657b903be20f75a93b54bae0171f
Willmuseve@ <willmuseve@gmail.com>
2023-10-17 02:58:11 -0700

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- a script thay lists alll records of the table second_table of the
+--database hbtn_oc_o
+-- results displays both score and the name
+-- records should be ordered by score.
+
+SELECT score, name FROM second_table ORDER BY score DESC;
