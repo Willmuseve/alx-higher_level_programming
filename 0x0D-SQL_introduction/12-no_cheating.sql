d249050f157dd7117abf15549a8d9e6febd0e7b0
Willmuseve@ <willmuseve@gmail.com>
2023-10-17 03:08:54 -0700

    No cheating

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- a script that updates the scores of bob to 10 in second_table
+-- you are notallowed to use Bob's ID VALUE ONLY THE NAME 
+
+UPDATE second_table SET score = 10 WHERE name = "Bob";
