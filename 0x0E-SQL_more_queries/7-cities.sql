ae3f808fc7541f25050606661a5791918d79b745
Willmuseve@ <willmuseve@gmail.com>
2023-10-18 03:16:48 -0700

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+--a script that creates the database hbtn_0d_usa and the table cities
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO INCREMENT PRIMARY
+KEY UNIQUE NOT NULL, state_id INT NOT NULL, name VARCHAR(256),
+FOREIGN KEY(state_id) REFERENCES states(id));
