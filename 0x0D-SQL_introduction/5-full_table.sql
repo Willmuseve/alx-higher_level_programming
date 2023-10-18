cb6053e6f7c4bd36c295165383c54d3a97d08c6d
Willmuseve@ <willmuseve@gmail.com>
2023-10-18 01:31:50 -0700

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
 --a script that prints the full description of the table of the 
 --first_table from the dtabase hbtn_0c_0
 
-SHOW CREATE TABLE first_table;
+SELECT COLUMN_NAME, DATA_TYPE, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY,
+COLUMN_DEFAULT, EXTRA
+       FROM information_schema.COLUMNS
+       WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
