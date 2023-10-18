6691b80498e17210656d2d43956e2b2e10cd2a0a
Willmuseve@ <willmuseve@gmail.com>
2023-10-18 04:09:27 -0700

    ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,3 @@
---creates the table id_not_null on your MySQL server.
+--creates the table id_not_null on your MySQL server
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.id_not_null (
-  id INT DEFAULT 1,
-  name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
