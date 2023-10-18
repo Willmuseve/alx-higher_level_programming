5057099f510b5ce75b85bae6050b705b08249659
Willmuseve@ <willmuseve@gmail.com>
2023-10-18 04:20:14 -0700

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,9 @@
 --a script that creates the database hbtn_0d_usa and the table cities
+-- cities description:
+-- 	  id INT unique, auto generated, can’t be null and is a primary key
+--	  state_id INT, can’t be null and must be a FOREIGN KEY
+-- 	  name VARCHAR(256) can’t be null
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO INCREMENT PRIMARY
-KEY UNIQUE NOT NULL, state_id INT NOT NULL, name VARCHAR(256),
-FOREIGN KEY(state_id) REFERENCES states(id));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO INCREMENT PRIMARY KEY UNIQUE NOT NULL, state_id INT NOT NULL, name VARCHAR(256),
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
