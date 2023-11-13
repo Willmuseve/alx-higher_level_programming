337980ba4e04bc0e1cac87bf6cbc1771e35b6624
Willmuseve <willmuseve@gmail.com>
2023-11-13 19:44:21 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// a program that Prints 3 lines
+// The first line: C is fun
+// The second line: Python is cool
+// The third line: JavaScript is am
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
