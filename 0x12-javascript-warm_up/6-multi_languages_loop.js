10325f305b4804898f8476b9cee7ee86a858762a
Willmuseve <willmuseve@gmail.com>
2023-11-13 20:23:16 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// A script that  Uses a for loop to prints contents
+// of an array of strings
+
+const Arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(Arr[i]);
+}
