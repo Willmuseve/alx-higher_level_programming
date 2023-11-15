07d531a0ec235dec3c9b3962314e017add950af7
Willmuseve <willmuseve@gmail.com>
2023-11-15 21:28:07 +0000

    Increment object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+//Update script
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
