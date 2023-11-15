7bb0460e66aff844c7a3a108c3fcdddcfa6af248
Willmuseve <willmuseve@gmail.com>
2023-11-15 20:28:19 +0000

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+//a program that Computes factorial of a number n
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const numb = parseInt(process.argv[2]);
+const ret = factorial(ret);
+
+console.log(ret);
