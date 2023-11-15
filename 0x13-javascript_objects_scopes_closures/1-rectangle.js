9cf3790354f25a6e6b4d2e197e3cce0576e8d27c
Willmuseve <willmuseve@gmail.com>
2023-11-15 21:50:03 +0000

    Rectangle #1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 
-//Check the parameters provided
- 
+/*
+ * a class Rectangle that defines a rectangle:
+ * You must use the class notation for defining your class
+ * The constructor must take 2 arguments w and h
+*/
+
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+    if (w === 0 || h === 0 || w < 0 || h < 0) {
+      return {};
+    } else {
       this.width = w;
       this.height = h;
     }
   }
 }
+
 module.exports = Rectangle;
