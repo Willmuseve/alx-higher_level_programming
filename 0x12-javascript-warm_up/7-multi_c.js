a3921d55ad55d5923e845e24b1f02701640d04fc
Willmuseve <willmuseve@gmail.com>
2023-11-15 18:57:31 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,14 @@
 //a script that prints x times "C is fun"
 
 const numb = process.argv[2];
+const isnts = parseInt(numb);
 
-if (!parseInt(numb) {
+if (Number.isNaN(insts)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < numb; i++) {
+  let x = insts;
+  while (x > 0) {
     console.log('C is fun');
+    i--;
   }
 }
