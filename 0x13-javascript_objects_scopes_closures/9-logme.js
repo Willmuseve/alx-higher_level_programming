525777aa847df81fb89271855572da0d134aa3ac
Willmuseve <willmuseve@gmail.com>
2023-11-17 19:36:19 +0000

    Log me

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-/* a script that prints the number of arguments already printed
-* and the new argument value
-*/
+// a script that prints the number of arguments already printed
+// and the new argument value
 
-let count = 0;
+let c = 0;
 
 exports.logMe = function (item) {
-  console.log(`${count}: ${item}`);
-  count++;
+  console.log(`${c}: ${item}`);
+  c++;
 }
