bccd4eaf9a386bf4925a00ab90f8d0fd453ec2c5
Willmuseve@ <willmuseve@gmail.com>
2023-11-19 18:42:20 -0800

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     """
     Access the db to get state
     """
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
