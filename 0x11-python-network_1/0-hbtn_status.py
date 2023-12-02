ea6a917961839caddb7ded8401bcd22027e89193
Willmuseve <willmuseve@gmail.com>
2023-12-01 17:19:47 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A Python script Fetches https://intranet.hbtn.io/status."""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+        body = response.read()
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
