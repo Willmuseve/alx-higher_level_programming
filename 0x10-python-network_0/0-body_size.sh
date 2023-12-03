3eb802a2b3ba30a6fbf8b13297ae271be884ed8b
Willmuseve <willmuseve@gmail.com>
2023-12-02 23:31:17 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that uses curl to send a request to a
-# cURL provided as arg and displays size of the body of the response
+# A script that uses curl to send a request
 curl -s "$1" | wc -c
