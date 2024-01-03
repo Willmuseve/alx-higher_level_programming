e9df084f35a89379dd901051205b06f01acde08c
Willmuseve <willmuseve@gmail.com>
2024-01-03 02:59:19 -0800

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+//declarations
+const request = require('request');
+const link = process.argv[2];
+
+//code
+request.get(link, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const DoneT = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!DoneT[todo.userId]) {
+        DoneT[todo.userId] = 1;
+      } else {
+        DoneT[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(DoneT);
+});
